9b3b6ef9eff8d007da70bfa46e1f0df444d7708a
Kobayashi Takahiro <koba925@gmail.com>
2023-09-11 21:51:44 +0900

    Semistandard

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const { App } = require("@slack/bolt");
+const { App } = require('@slack/bolt');
 
 const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    appToken: process.env.SLACK_APP_TOKEN,
-    socketMode: true
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true
 });
 
-app.event("app_home_opened", async ({ event, say }) => {
-    await say("Hello, world!");
+app.event('app_home_opened', async ({ event, say }) => {
+  await say('Hello, world!');
 });
 
 (async () => {
-    await app.start(process.env.PORT || 3000);
-    console.log("App is running in socket mode.");
+  await app.start(process.env.PORT || 3000);
+  console.log('App is running in socket mode.');
 })();
